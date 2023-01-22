ce9b62cbf8b3cefcf7fb02384ebd1ef71cd1bdfe
erickssguerra <erickssguerra@gmail.com>
2023-01-21 23:34:44 -0300

    feat: unenroll customer

--- src/controllers/enrollments.controllers.ts
+++ src/controllers/enrollments.controllers.ts
@@ -7,11 +7,23 @@ export async function enrollCustomer(req: Request, res: Response) {
   try {
     const enrollmentDetails: EnrollmentDetails =
       await enrollmentsRepository.enroll(enrollment);
-    res
-      .status(200)
-      .send({
-        message: `Success: ${enrollmentDetails.customer} was enrolled into ${enrollmentDetails.course}!`,
-      });
+    res.status(200).send({
+      message: `Success: ${enrollmentDetails.customer} was enrolled into ${enrollmentDetails.course}!`,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.detail });
+  }
+}
+
+export async function unenrollCustomer(req: Request, res: Response) {
+  const unenrollment: Enrollment = res.locals.validEnrollment;
+  try {
+    const unenrollmentDetails: EnrollmentDetails =
+      await enrollmentsRepository.unenroll(unenrollment);
+    res.status(200).send({
+      message: `Success: ${unenrollmentDetails.customer} was unerolled from ${unenrollmentDetails.course}!`,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).send({ message: err.detail });
--- src/repositories/enrollments.repository.ts
+++ src/repositories/enrollments.repository.ts
@@ -4,7 +4,7 @@ import { Enrollment, EnrollmentDetails } from "../protocols/enrollment.js";
 async function enroll(enrollment: Enrollment) {
   const { rows } = await connectionDB.query(
     `
-      WITH enrolled_customer as (
+    WITH enrolled_customer as (
         INSERT INTO "enrollments" ("customer_id", "course_id") 
         VALUES ($1, $2) RETURNING * ) 
     SELECT
@@ -18,6 +18,30 @@ async function enroll(enrollment: Enrollment) {
   return rows[0] as EnrollmentDetails;
 }
 
+async function unenroll(unerollment: Enrollment) {
+  const { rows } = await connectionDB.query(
+    `
+    WITH enrolled_customer as (
+      DELETE FROM "enrollments" 
+      WHERE "customer_id" = $1 AND "course_id" = $2 
+      RETURNING * ) 
+    SELECT
+      customers.name AS customer,
+      courses.name AS course
+    FROM enrolled_customer
+    JOIN customers ON enrolled_customer.customer_id = customers.id
+    JOIN courses ON enrolled_customer.course_id = courses.id;
+    ;`,
+    [unerollment.customer_id, unerollment.course_id]
+  );
+  rows[0] as EnrollmentDetails;
+  if (!rows[0]) {
+    throw { detail: `Enrollment doesn't exist.` } ;
+  }
+  return rows[0];
+}
+
 export const enrollmentsRepository = {
   enroll,
+  unenroll,
 };
