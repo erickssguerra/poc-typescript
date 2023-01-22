699e5a4b26a66042ad0626d79356c39d93bb4b2b
erickssguerra <erickssguerra@gmail.com>
2023-01-22 16:12:46 -0300

    feat: verify if course or customer exists

--- src/repositories/enrollments.repository.ts
+++ src/repositories/enrollments.repository.ts
@@ -1,7 +1,7 @@
 import connectionDB from "../database/connectionDB.js";
 import { Enrollment, EnrollmentDetails } from "../protocols/enrollment.js";
 
-async function enroll(enrollment: Enrollment) {
+async function enroll(enrollment: Enrollment): Promise<EnrollmentDetails> {
   const { rows } = await connectionDB.query(
     `
     WITH enrolled_customer as (
@@ -15,10 +15,23 @@ async function enroll(enrollment: Enrollment) {
     JOIN courses ON enrolled_customer.course_id = courses.id;`,
     [enrollment.customer_id, enrollment.course_id]
   );
-  return rows[0] as EnrollmentDetails;
+  const confirmation: EnrollmentDetails = rows[0];
+  return confirmation;
 }
 
 async function unenroll(unerollment: Enrollment) {
+  await connectionDB.query(`
+  DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT * FROM customers WHERE customers.id = ${unerollment.customer_id} ) THEN
+      RAISE USING DETAIL = 'customer_id not found';
+    ELSE 
+      IF NOT EXISTS (SELECT * FROM courses WHERE courses.id = ${unerollment.course_id}) THEN
+        RAISE USING DETAIL = 'course_id not found';
+      END IF;
+    END IF;
+  END $$;`);
+
   const { rows } = await connectionDB.query(
     `
     WITH enrolled_customer as (
@@ -34,11 +47,13 @@ async function unenroll(unerollment: Enrollment) {
     ;`,
     [unerollment.customer_id, unerollment.course_id]
   );
-  rows[0] as EnrollmentDetails;
-  if (!rows[0]) {
-    throw { detail: `Enrollment doesn't exist.` } ;
+  const confirmation: EnrollmentDetails = rows[0];
+  if (!confirmation) {
+    throw {
+      detail: `The customer is not enrolled in this course.`,
+    };
   }
-  return rows[0];
+  return confirmation;
 }
 
 export const enrollmentsRepository = {
